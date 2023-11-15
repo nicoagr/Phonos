a1e8947d2d93e90e1bd8a418beea0d71863cc752
MartinH13 <martin.i.horsfield@gmail.com>
2023-11-15 17:53:16 +0100

    initAudio event listeners

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 class App {
+
     audio;
     blob;
     state;
+
+
     constructor() {
 
     }
@@ -10,6 +13,21 @@ class App {
     }
 
     initAudio() {
+        this.audio = new Audio();
+        document.body.appendChild(this.audio);
+
+        document.getElementById('audio').addEventListener('onloadedmetadata', () => {
+            console.log("onloadedmetadata")
+        });
+        document.getElementById('audio').addEventListener('ondurationchange', () => {
+            console.log("ondurationchange")
+        });
+        document.getElementById('audio').addEventListener('ontimeupdate', () => {
+            console.log("ontimeupdate")
+        });
+        document.getElementById('audio').addEventListener('onended', () => {
+           console.log("onended")
+        });
     }
 
     loadBlob() {
