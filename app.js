4976ac54d3b601c008ef02aaae791790031553bf
Nicolás Aguado <61473739+nicoagr@users.noreply.github.com>
2024-04-26 09:54:09 +0000

    fix post request bug

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false , limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
